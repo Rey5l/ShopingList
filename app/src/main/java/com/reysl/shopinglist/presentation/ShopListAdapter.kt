0f5b3c8ac1b7d88c6a686047d5612063cdf8c40c
Reysl <rasulnasaraliev@gmail.com>
2024-05-19 16:02:35 +0300

    adding a function to remove an element from list

--- app/src/main/java/com/reysl/shopinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/reysl/shopinglist/presentation/ShopListAdapter.kt
@@ -70,7 +70,6 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     companion object {
         const val VIEW_TYPE_ENABLED = 100
         const val VIEW_TYPE_DISABLED = 101
-
         const val MAX_POOL_SIZE = 20
     }
 
